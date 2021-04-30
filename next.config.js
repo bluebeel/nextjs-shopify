796eb98b0aabe79962d12314a898c5e4c25b10e1
James Pamplin <james@sellalong.com>
2021-04-30 12:44:10 +0100

    refactor: 💡 use process.env for env vars created by shopify-cli

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-const { parsed: localEnv } = require("dotenv").config();
 const withCSS = require("@zeit/next-css");
 
-const webpack = require("webpack");
-const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
-
 module.exports = withCSS({
-  webpack: config => {
-    const env = { API_KEY: apiKey };
-    config.plugins.push(new webpack.DefinePlugin(env));
-    return config;
-  }
+  env: {
+    API_KEY: process.env.SHOPIFY_API_KEY,
+    HOST: process.env.HOST,
+  },
 });
