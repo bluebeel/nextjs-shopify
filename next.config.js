c1c9bcdf48359b36830a8dbb6769de17cb816295
James Pamplin <james@sellalong.com>
2021-04-30 17:11:57 +0100

    fix: 🐛 redirect /auth to /api/shopify/auth for shopify-cli

--- next.config.js
+++ next.config.js
@@ -5,4 +5,16 @@ module.exports = withCSS({
     API_KEY: process.env.SHOPIFY_API_KEY,
     HOST: process.env.HOST,
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/auth",
+        destination: "/api/shopify/auth",
+
+        // Set this to true if you're sure you'll never use the /auth page (beware, permanent redirects as they are difficult to invalidate in clients)
+        permanent: false,
+      },
+    ];
+  },
 });
